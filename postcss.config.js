135d049de8be34bccc45d3baa45b6441303f2172
Pablo Martínez Ruano <pableitories@gmail.com>
2020-05-05 18:04:58 +0200

    add proyect files

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+          }),
+          require('cssnano')({
+            preset: 'default',
+        })  
+    ]
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        primary: '#f3c614',
+        secondary: '#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
